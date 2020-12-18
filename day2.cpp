f7b67f10d34cf2e79fed3514bc4ac882227d33d6
Aniket Pingley <aniket.pingley@gmail.com>
2020-12-18 12:32:04 +0530

    added example of factory pattern using example of wrapper classes for third party libraries

---
+++ day2.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+// OPEN SSL simulation starts
+struct XBio{
+    double x;
+    double y;
+};
+
+char* get_sequence_sl(int y){
+    return "get_sequence_sl_openssl";
+}
+
+void set_xbio_sequence(XBio* n){}
+// OPEN SSL ends here
+
+// Cisco's internal security library starts
+class CiscoSec{
+    public:
+        void init(const std::string& val){}
+        std::string process(){ 
+            return "CiscoSec process";
+        }
+};
+// Cisco's internal security library end
+
+
+class ISecurityWrapper{
+    public:
+        /**
+        */ 
+        virtual void init(const std::string& inpStr) = 0;
+        virtual std::string process() = 0;
+};
+
+class OpenSSLWrapper : public ISecurityWrapper{
+    public:
+        virtual void init(const std::string& inpStr){
+            set_xbio_sequence(nullptr);
+        }
+
+        std::string process(){
+             std::string d(get_sequence_sl(10));
+            return d;
+        }
+};
+
+class CiscoSecWrapper : public ISecurityWrapper{
+    CiscoSec *csec;
+    public:
+        CiscoSecWrapper(){
+            csec = new CiscoSec();
+        }
+        
+        void init(const std::string& inpStr){
+            csec->init(inpStr);
+        }
+
+        std::string process(){
+             return csec->process();
+        }
+};
+
+
+// #include <openssl.h>
+class WrapperSecurity{
+    private:
+        std::string _val;
+        double _a;
+        int _b;
+    public:
+        WrapperSecurity(const std::string& val){
+            _val = val;
+            // some computation on val.
+            _a = 10;
+            _b = 9;
+        }
+
+        void initSecurityLib(){
+            set_xbio_sequence(nullptr);
+        }
+
+        std::string getSequence(){
+            std::string d(get_sequence_sl(_b));
+            return d;
+        }
+
+};
+
+
+// some other cpp file
+int main(){
+    //WrapperSecurity s("eeqfeqf");
+    //s.initSecurityLib();
+    //std::cout << s.getSequence() << std::endl;
+    int in;
+    std::cin >> in;
+    // factory pattern 
+    // factory method would have a function that takes
+    // input and determines the concrete class
+    ISecurityWrapper *sWrapper = nullptr;
+    if(1 == in) // Factory
+        sWrapper = new OpenSSLWrapper();
+    if(2 == in)
+        sWrapper = new CiscoSecWrapper();
+
+    sWrapper->init("ewfewfe");
+    std::cout << sWrapper->process() << std::endl;
+
+    return 0;
+}
